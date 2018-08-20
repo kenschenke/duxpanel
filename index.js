0a6ea7a9b08f2af4a09d15593d3e088d8797bba6
Ken Schenke <kschenke@datafreight.com>
2018-08-20 16:34:42 -0500

    Add website-dist webpack and index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+export { DuxDialog } from './lib/DuxDialog';
+export { DuxOkDialog } from './lib/DuxOkDialog';
+export { DuxPanel } from './lib/DuxPanel';
+export { DuxProgressDialog } from './lib/DuxProgressDialog';
+export { DuxYesNoDialog } from './lib/DuxYesNoDialog';
+export { ProgressBarBS4 } from './lib/ProgressBarBS4';
---
+++ webpack.website-dist.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './website/index.jsx',
+    mode: "production",
+    output: {
+        path: path.resolve(__dirname, './website/dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify("production")
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: "duxpanel.css"
+        }),
+        new OptimizeCSSAssetsPlugin({}),
+        new CopyWebpackPlugin([
+            {from: './website/build/index.html', flatten:true}
+        ])
+    ],
+    module: {
+        rules: [
+            {
+                test: /(\.js|\.jsx)$/,
+                exclude: /(node_modules)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['env', 'stage-0', 'react']
+                }
+            },
+            {
+                test: /(\.html|\.txt)$/,
+                loader: 'raw-loader',
+                exclude: /(mindex.html)/
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    "css-loader"
+                ]
+            }
+        ]
+    }
+};
